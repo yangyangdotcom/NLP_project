0cdc17514b0d68b87f56129a567680ae69b2ae7f
yangyangdotcom <tanciyoung99@gmail.com>
2023-07-25 22:38:01 -0500

    added pre-commit configs

--- setup.py
+++ setup.py
@@ -6,18 +6,14 @@ from setuptools import find_namespace_packages, setup
 
 # Load packages from requirements.txt
 BASE_DIR = Path(__file__).parent
-with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+with open(Path(BASE_DIR, "requirements.txt")) as file:
     required_packages = [ln.strip() for ln in file.readlines()]
 
 docs_packages = ["mkdocs==1.3.0", "mkdocstrings==0.18.1"]
 
 style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
 
-test_packages = [
-    "pytest==7.1.2",
-    "pytest-cov==2.10.1",
-    "great-expectations==0.15.15"
-]
+test_packages = ["pytest==7.1.2", "pytest-cov==2.10.1", "great-expectations==0.15.15"]
 setup(
     name="bnai",
     version=0.1,
@@ -26,9 +22,10 @@ setup(
     author_email="tanciyoung99@gmail.com",
     url="https://madewithml.com/",
     python_requires=">=3.7",
+    packages=find_namespace_packages(),
     install_requires=[required_packages],
     extras_require={
-        "dev": docs_packages + style_packages + test_packages,
+        "dev": docs_packages + style_packages + test_packages + ["pre-commit==2.19.0"],
         "docs": docs_packages,
         "test": test_packages,
     },
